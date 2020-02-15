ee76b60d81d4296f3d2142e8f3b1db180564bc61
RyosukeHirose <hhh.ryosuke@gmail.com>
2020-02-15 06:41:02 +0000

    サンプルvuew画面作成

--- manager_project/urls.py
+++ manager_project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+
+import manager.views as manager_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^worker_list/', manager_view.WorkerListView.as_view())  # URLとViewを組み合わせる！
 ]
